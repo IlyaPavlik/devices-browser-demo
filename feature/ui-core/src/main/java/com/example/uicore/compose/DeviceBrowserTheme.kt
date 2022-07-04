af88909853d9973d2060106ceb2282e30ce796ed
Ilya Pavlik <polano256@gmail.com>
2022-07-04 10:23:57 +0600

    Add dark theme

--- feature/ui-core/src/main/java/com/example/uicore/compose/DeviceBrowserTheme.kt
+++ feature/ui-core/src/main/java/com/example/uicore/compose/DeviceBrowserTheme.kt
@@ -1,9 +1,21 @@
 package com.example.uicore.compose
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 @Composable
 fun DeviceBrowserTheme(content: @Composable () -> Unit) {
-    MaterialTheme(content = content)
+    val colors = if (isSystemInDarkTheme()) DarkColors else LightColors
+
+    MaterialTheme(
+        colors = colors,
+        content = content
+    )
 }
+
+private val LightColors = lightColors()
+
+private val DarkColors = darkColors()
